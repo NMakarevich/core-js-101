0fff669b1e00c888667d9bf0f9ec849e9f7664bc
Nikolai Makarevich <nikoladze529@mail.ru>
2022-01-07 20:28:01 +0300

    feat: solve part of promises tasks

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,8 +28,19 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((resolve, reject) => {
+    let answer = '';
+    if (typeof isPositiveAnswer !== 'boolean') {
+      reject(new Error('Wrong parameter is passed! Ask her again.'));
+    }
+    if (isPositiveAnswer === true) {
+      answer = 'Hooray!!! She said "Yes"!';
+    } else if (isPositiveAnswer === false) {
+      answer = 'Oh no, she said "No".';
+    }
+    resolve(answer);
+  });
 }
 
 
@@ -48,8 +59,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
@@ -71,8 +82,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
